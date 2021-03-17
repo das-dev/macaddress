39c38f9ab7944416969bf84a11429fa169b730ac
das <das.dev@pm.me>
2021-03-17 23:15:34 +0700

    Add more tested assertions

--- tests.py
+++ tests.py
@@ -4,13 +4,22 @@ from unittest.mock import patch, MagicMock
 
 from main import OUIList, OUIRemoteSrc, OUIJsonStorage, OctetsSet, MACAddress
 
+TEST_OUI_LIST = {
+    '002272': 'American Micro-Fuel Device Corp.',
+    '00D0EF': 'IGT'
+}
+TEST_NORMALIZED_OUI_LIST = {
+    '00:22:72': 'American Micro-Fuel Device Corp.',
+    '00:D0:EF': 'IGT'
+}
+
 
 class TestOUIList(unittest.TestCase):
 
     @patch('main.OUIRemoteSrc')
     @patch('main.OUIJsonStorage')
     def test_preloading_empty_oui_list_from_empty_src(self, mock_storage: MagicMock, mock_src: MagicMock) -> None:
-        mock_src.fetch.return_value = {'FFFFFF': 'Broadcast'}
+        mock_src.fetch.return_value = TEST_OUI_LIST
         mock_storage.load.return_value = {}
         oui_list = OUIList(mock_src, mock_storage)
 
@@ -20,26 +29,37 @@ class TestOUIList(unittest.TestCase):
     @patch('main.OUIRemoteSrc')
     @patch('main.OUIJsonStorage')
     def test_updated_oui_list_from_src(self, mock_storage: MagicMock, mock_src: MagicMock) -> None:
-        mock_src.fetch.return_value = {'FFFFFF': 'Broadcast'}
+        mock_src.fetch.return_value = TEST_OUI_LIST
         mock_storage.load.return_value = {}
         oui_list = OUIList(mock_src, mock_storage)
         oui_list.update()
 
-        self.assertDictEqual(oui_list.data, {'FF:FF:FF': 'Broadcast'})
+        self.assertDictEqual(oui_list.data, TEST_NORMALIZED_OUI_LIST)
 
-        self.assertEqual(oui_list.lookup_by_oui('FFFFFF'), 'Broadcast')
-        self.assertEqual(oui_list.lookup_by_oui('FF:FF:FF'), 'Broadcast')
+        self.assertEqual(oui_list.lookup_by_oui('00D0EF'), 'IGT')
+        self.assertEqual(oui_list.lookup_by_oui('00-D0-EF'), 'IGT')
+        self.assertEqual(oui_list.lookup_by_oui('00:D0:EF'), 'IGT')
         self.assertIsNone(oui_list.lookup_by_oui('00:00:00'))
-        with self.assertRaises(ValueError):
-            oui_list.lookup_by_oui('FF:FF:F')
 
-        self.assertEqual(oui_list.lookup_by_mac('FFFFFFFFFFFF'), 'Broadcast')
-        self.assertEqual(oui_list.lookup_by_mac('FF:FF:FF:FF:FF:FF'), 'Broadcast')
+        with self.assertRaises(ValueError):
+            oui_list.lookup_by_oui('00:D0:E')
+        with self.assertRaises(ValueError):
+            oui_list.lookup_by_oui('0:D0:EF')
+
+        self.assertEqual(oui_list.lookup_by_mac('00D0EFFFFFFF'), 'IGT')
+        self.assertEqual(oui_list.lookup_by_mac('00-D0-EF-FF-FF-FF'), 'IGT')
+        self.assertEqual(oui_list.lookup_by_mac('00D0.EFFF.FFFF'), 'IGT')
+        self.assertEqual(oui_list.lookup_by_mac('00:D0:EF:FF:FF:FF'), 'IGT')
         self.assertIsNone(oui_list.lookup_by_mac('00:00:00:00:00:00'))
-        with self.assertRaises(ValueError):
-            oui_list.lookup_by_mac('FF:FF:FF')
 
-        mock_storage.dump.assert_called_once_with({'FF:FF:FF': 'Broadcast'})
+        with self.assertRaises(ValueError):
+            oui_list.lookup_by_mac('00:D0:EF')
+        with self.assertRaises(ValueError):
+            oui_list.lookup_by_mac('00:D0:EF:FF:FF:FF:F')
+        with self.assertRaises(ValueError):
+            oui_list.lookup_by_mac('00:D0:EF:FF:FF:FFF')
+
+        mock_storage.dump.assert_called_once_with(TEST_NORMALIZED_OUI_LIST)
         mock_storage.load.assert_called_once()
 
 
@@ -66,10 +86,7 @@ class TestOUIRemoteSrc(unittest.TestCase):
         mock_response.read.return_value = self.CSV.strip()
         mock_request.urlopen.return_value.__enter__.return_value = mock_response
 
-        self.assertDictEqual(OUIRemoteSrc().fetch(), {
-            '002272': 'American Micro-Fuel Device Corp.',
-            '00D0EF': 'IGT'
-        })
+        self.assertDictEqual(OUIRemoteSrc().fetch(), TEST_OUI_LIST)
 
 
 class TestMACAddress(unittest.TestCase):
@@ -89,4 +106,7 @@ class TestMACAddress(unittest.TestCase):
             MACAddress('FF:FF:FF')
 
     def test_valid_mac_address(self) -> None:
+        self.assertEqual(str(MACAddress('FFFFFFFFFFFF')), 'FF:FF:FF:FF:FF:FF')
+        self.assertEqual(str(MACAddress('FF-FF-FF-FF-FF-FF')), 'FF:FF:FF:FF:FF:FF')
+        self.assertEqual(str(MACAddress('FFFF.FFFF.FFFF')), 'FF:FF:FF:FF:FF:FF')
         self.assertEqual(str(MACAddress('FF:FF:FF:FF:FF:FF')), 'FF:FF:FF:FF:FF:FF')
